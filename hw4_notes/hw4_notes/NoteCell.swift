a65e4e1dffd25c2215c65385bdd8f0e6b5511ffa
artgas1 <artgas1@email>
2021-10-31 22:43:05 +0300

    Add collection view and embed in navigation

---
+++ hw4_notes/hw4_notes/NoteCell.swift
@@ -0,0 +1,10 @@
+import Foundation
+import UIKit
+
+class NoteCell: UICollectionViewCell {
+    
+    @IBOutlet weak var title: UILabel!
+    @IBOutlet weak var body: UILabel!
+    
+    
+}
--- hw4_notes/hw4_notes/ViewController.swift
+++ hw4_notes/hw4_notes/ViewController.swift
@@ -2,11 +2,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        collectionView.delegate = self
+        collectionView.dataSource = self
     }
 
-
+    
 }
 
+extension ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "NoteCell", for: indexPath) as! NoteCell
+        cell.title.text = "zzz"
+        cell.body.text = "uuu"
+        return cell
+    }
+}
